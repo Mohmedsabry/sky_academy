ef9bc4c3ff83ca01d11868c58635487c7e12b8b1
MohmedSabry <mayjak60@gmail.com>
2026-08-31 18:19:34 +0300

    implement splash screen, refactor MainActivity to support SplashScreen API, and update theme and resource configurations for the student module

--- student/src/main/java/com/centerk/MainActivity.kt
+++ student/src/main/java/com/centerk/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.centerk
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,10 +11,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.core.ui.theme.CenteryTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                false
+            }
+        }
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
